ccf0ff713a99bd07ca5f6fed2e476a114184a9a4
Saplace <8475900@qq.com>
2018-05-08 13:13:10 +0100

    Revert "parallelized code"
    This reverts commit 6dea5be34bb0170b10f4d4613499693e6fa10bbf.

--- Parallelized code/flat MPI/env.sh
+++
@@ -1,7 +0,0 @@
-# Add any `module load` or `export` commands that your code needs to
-# compile and run to this file.
-module load CUDA/8.0.44
-module use /mnt/storage/scratch/jp8463/modules/modulefiles
-module load clang-ykt/2017-07-24
-module load iimpi/2017.01-GCC-5.4.0-2.26
-module load languages/anaconda2
